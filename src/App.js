3f058a30cc2ea68f271f95b24a55114ca414cb7b
Sergio Mosquera <smosqueram001@hotmail.com>
2021-05-10 14:01:43 -0500

    useeffect permite manejar efectos que van a ser transmitido dentro nuestros componentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Characters from './components/Characters';
 import './App.css';
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <Characters/>
       <h1>Hola Mundo</h1>
       <h3>Este curso lo sigo para aprender pero ademas busco llenar mi repositorio en el tiempo ...</h3>
     </div>
