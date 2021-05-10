038a6425d7099e9be5dfda21826b505f4308cbf3
Sergio Mosquera <smosqueram001@hotmail.com>
2021-05-10 03:01:19 -0500

    etapa 1 del proyecto useState estado en componentes creados como funciones

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola Mundo</h1>
+      <h3>Este curso lo sigo para aprender pero ademas busco llenar mi repositorio en el tiempo ...</h3>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <h1>ReacHooks</h1>
+            <button type="button">DarkMode</button>
+        </div>
+    );
+}
+
+export default Header;
