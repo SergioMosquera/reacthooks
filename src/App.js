82ec47e302b4ccaaa3a755317f41e39c856b27db
Sergio Mosquera <smosqueram001@hotmail.com>
2021-05-10 03:45:31 -0500

    Estado en componentes creado como funciones

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import React from 'react';
+import Header from './components/Header';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1>Hola Mundo</h1>
       <h3>Este curso lo sigo para aprender pero ademas busco llenar mi repositorio en el tiempo ...</h3>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const Header = () => {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const handleClick = () => {
+        setDarkMode(!darkMode);
+    }
     return (
         <div className="Header">
-            <h1>ReacHooks</h1>
-            <button type="button">DarkMode</button>
+            <h1>React Hooks</h1>
+            <button type="button" onClick={handleClick}>{darkMode ? 'Dark Mode' : 'Light Mode'}</button>
+            <button type="button" onClick={ () => setDarkMode (!darkMode) } > {darkMode ? 'Dark Mode' : 'Light Mode'}</button>
         </div>
     );
 }
